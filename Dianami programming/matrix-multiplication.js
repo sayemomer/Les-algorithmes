4006c4b3c0b3a2f56b63a6bb815e01f1b6d87e43
omersayem007 <omer_sayem@yahoo.com>
2017-02-23 16:34:17 +0600

    Update matrix-multiplication.js

--- Dianami programming/matrix-multiplication.js
+++ Dianami programming/matrix-multiplication.js
@@ -1,18 +1,19 @@
 function matrixmultiply( A, B)
 {
-  var c=[];
+  var c =[[0,0],[0,0]];
   if( A[0].length !=B.length ) // if col[A]!=row[B] // CR soman hobe
   
   return " Incompatible dimension";
+  
   else
+  
   {
     for( var i=0;i<A.length;i++) //  array A er row  ,matrix mul:RC
     {
       for(var j=0;j<B[0].length;j++) // array B er column
       {
-        c[i][j]=0;
-        
-        for( var k=0; k<A[0].length;k++)
+         
+          for( var k=0; k<A[0].length;k++)
         {
           c[i][j]= c[i][j] + A[i][k]*B[k][j];
         }
@@ -20,7 +21,7 @@ function matrixmultiply( A, B)
     }
   }
   
-  return C;
+  return c;
 
 }
 
