9fda210ee0fcf4b0a23383c70f2a23a2ab28af02
omersayem007 <omer_sayem@yahoo.com>
2017-03-26 02:28:17 +0600

    Update bfs.cpp

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -2,10 +2,26 @@
 #include <queue>
 using namespace std;
 int graph[1000][1000];
-int vertices ;
+int vertices , start, finish;
 string color[100];
 int d[50];
 int p[50];
+
+void print_path(int start , int finish)
+{
+   if( start == finish )
+   {
+       cout<<" "<<p[start]<<endl;
+   }
+   else if( p[finish]== NULL) {
+        cout<<"No path from:"<<start<<"to"<<finish<<"exits";
+   }
+   else
+    print_path( start , p[finish]);
+   cout<<" "<<p[finish];
+
+
+}
 
 int bfs( int source)
 {
@@ -26,6 +42,8 @@ int bfs( int source)
   while( !myqueue.empty() )
     {
         u =myqueue.front();
+        myqueue.pop();
+
             for( int v=0 ;v<vertices ;v++)
                 {
                     if( graph[u][v]==1 && color[v]=="white")
@@ -45,7 +63,7 @@ int bfs( int source)
 
 int main()
 {
-    int row,col,source;
+    int row,col,source,v1,v2;
     cout<<"Enter the number of vertices : "<<endl;
     cin>>vertices;
     row=vertices;
@@ -76,9 +94,34 @@ int main()
 
         bfs(source);
 
-    for(int i=0;i<vertices;i++)
-    {
-       cout<<" "<<color[i];
-    }
+        for( int i=0;i<vertices;i++)
+  {
+     cout<<"Distance from: "<<source<<" to "<<i<<" is "<<d[i]<<endl ;
+  }
+
+  cout<<"Enter the start and end vertex to print path :"<<endl;
+  cin>>start>>finish;
+
+  print_path( start ,finish );
+
+cout<<endl;
+cout<<"Enter vertices to find distance:"<<endl;
+cin>>v1>>v2;
+  bfs(v1);
+   cout<<"Distance from: "<<v1<<" to "<<v2<<" is "<<d[v2];
+
     return 0;
 }
+
+/*
+9
+0 1 0 1 0 0 0 0 0
+0 0 1 0 1 0 0 0 0
+0 0 0 0 0 0 0 0 0
+0 1 0 0 1 0 0 0 0
+0 0 1 0 0 0 1 0 1
+0 0 0 1 0 0 0 1 0
+0 0 0 0 0 1 0 0 1
+0 0 0 0 0 0 1 0 0
+0 0 0 0 0 0 0 1 0
+*/
