03a04f189c970d302b94ff542499c06ac04bc323
sayemomer <omer_sayem@yahoo.com>
2021-02-21 22:43:39 +0600

    bfs and word ladder

--- Graph/Bredth-first-seach.js
+++ Graph/Bredth-first-seach.js
@@ -1,54 +1,49 @@
-function bfs(graph,source)
-{
-  var color=[];
-   var queqe=[];
-   var d=[];
-   var p=[];
-   var u ;
-   d[0]=0;
-    queqe.push(source);
- 
-  
-  for(var i=0;i<graph.length;i++)
-    {
-      color[i]="white";
+function bfs(graph, source) {
+  var color = [];
+  var queqe = [];
+  var d = [];
+  var p = [];
+  var u;
+  d[0] = 0;
+  queqe.push(source);
+
+
+  for (var i = 0; i < graph.length; i++) {
+    color[i] = "white";
+  }
+
+
+  while (queqe.length !== 0) {
+    u = queqe.shift();
+    for (var v = 0; v < graph.length; v++) {
+      if (graph[u][v] == 1 && color[v] == "white") {
+        color[v] = "gray";
+        d[v] = d[u] + 1;
+        p[v] = u;
+        queqe.push(v);
+
+      }
     }
-  
-  
-  while( queqe.length !== 0)
-    {
-        u =queqe.shift();
-       for( var v=0 ;v<graph.length ;v++)
-       {
-         if( graph[u][v]==1 &&color[v]=="white")
-         {
-           color[v]="gray";
-           d[v]=d[u]+1;
-           p[v]=u;
-           queqe.push(v);
-           
-         }
-       }
-     color[u]="black";  
-   }
-  
-   
-  
-print(d);
- 
+    color[u] = "black";
+  }
+
+
+
+  console.log(d);
+
 }
 
-var graph=[
-  [0,1,0,1,0,0,0,0,0],
-  [0,0,1,0,1,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,1,0,0,1,0,0,0,0],
-  [0,0,1,0,0,0,1,0,1],
-  [0,0,0,1,0,0,0,1,0],
-  [0,0,0,0,0,1,0,0,1],
-  [0,0,0,0,0,0,1,0,0],
-  [0,0,0,0,0,0,0,1,0]
-  
+var graph = [
+  [0, 1, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0, 1, 0, 1],
+  [0, 0, 0, 1, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 0]
+
 ];
 
-console.log(bfs(graph,0));
+console.log(bfs(graph, 0));
