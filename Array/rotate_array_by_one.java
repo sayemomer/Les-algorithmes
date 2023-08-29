3e4298d6f5cd03358e6f426b5b6add26115d7b02
sayemomer <omer_sayem@yahoo.com>
2023-08-29 19:49:12 -0400

    unfinished array rotation

---
+++ Array/rotate_array_by_one.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class rotate_array_by_one {
+
+    public static void rotateArray(int[] arr){
+
+        // Write - Your - Code    
+        //first we can create a new array on the same length
+        //then we can relocate the value according to the index value which is subtructed from the value length of the array
+
+        
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+
+        rotateArray(arr);
+    }
+    
+}
