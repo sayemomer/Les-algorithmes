1dd817f04e6bc10dfa546a870ca2fc505d372d5f
sayemomer <omer_sayem@yahoo.com>
2024-07-25 08:27:42 -0400

    same tree tree problem

--- Tree/same_tree.java
+++ Tree/same_tree.java
@@ -1,9 +1,41 @@
 package Tree;
 
+import java.util.Scanner;
+
 public class same_tree {
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if(p == null && q == null){
+            return true;
+        }
+
+        if(p != null && q!= null && p.val == q.val){
+            return isSameTree(p.left , q.left ) && isSameTree(p.right , q.right);
+        }else {
+            return false;
+        }
+
+    }
 
 
     public static void main(String[] args) {
+
+        try(Scanner sc = new Scanner(System.in)) {
+
+            String[] firstTree_s = sc.nextLine().split(" ");
+            String[] secondTree_s = sc.nextLine().split(" ");
+
+            TreeNode firstTree = TreeNode.insertLevelOrder(firstTree_s);
+            TreeNode secondTree = TreeNode.insertLevelOrder(secondTree_s);
+
+            System.out.println(isSameTree(firstTree,secondTree));
+
+
+
+        }catch (Exception e){
+
+        }
         
     }
     
