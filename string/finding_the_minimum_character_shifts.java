7a9952f44358d517f058c616dcb29ca0938657fc
sayemomer <omer_sayem@yahoo.com>
2024-09-11 20:06:48 -0400

    min character shift and valid mountain

--- string/finding_the_minimum_character_shifts.java
+++ string/finding_the_minimum_character_shifts.java
@@ -1,2 +1,78 @@
-package string;public class finding_the_minimum_character_shifts {
+package string;
+
+import java.util.Scanner;
+
+public class finding_the_minimum_character_shifts {
+
+    public static int min_char_shift_v2(String src,String tar){
+
+        int result = -1;
+
+        if(src.length() != tar.length()){
+            result=-1;
+            return result;
+        }
+
+        if(src.equals(tar)){
+            result =0;
+            return result;
+        }
+
+        String concated = src.concat(src);
+
+        result = concated.indexOf(tar);
+
+        return result;
+
+    }
+
+    public static int min_char_shift(String src,String tar){
+
+        int result = -1;
+
+        if(src.length() != tar.length()){
+            result=-1;
+            return result;
+        }
+
+        if(src.equals(tar)){
+            result =0;
+            return result;
+        }
+
+        String concated = src.concat(src);
+
+        int src_p = 0;
+        int tar_p = tar.length();
+
+        while (src_p < concated.length() && tar_p < concated.length()){
+
+            //in java substring the last one is exclusive
+            String sub_str = concated.substring(src_p,tar_p);
+            if(sub_str.equals(tar)){
+                result = src_p;
+                break;
+            }
+            src_p++;
+            tar_p++;
+        }
+
+        return result;
+
+    }
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)) {
+
+            String[] line = sc.nextLine().split(" ");
+            String src = line[0];
+            String tar = line[1];
+
+//            System.out.println(min_char_shift(src,tar));
+            System.out.println(min_char_shift_v2(src,tar));
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
